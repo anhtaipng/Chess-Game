b19fe3f1af3a2580b8a5f0fcd31c2ed62aacb878
AnhTai <anhtaipng@gmail.com>
2021-09-09 17:43:36 +0700

    add id_user to response api login

--- authenticate/src/main/java/vn/gihot/chess/authenticate/model/JwtResponse.java
+++ authenticate/src/main/java/vn/gihot/chess/authenticate/model/JwtResponse.java
@@ -2,13 +2,20 @@ package vn.gihot.chess.authenticate.model;
 
 public class JwtResponse {
     private static final long serialVersionUID = -8091879091924046844L;
+    private final String id_user;
     private final String jwttoken;
 
-    public JwtResponse(String jwttoken) {
+
+    public JwtResponse(String id_user, String jwttoken) {
+        this.id_user = id_user;
         this.jwttoken = jwttoken;
     }
 
     public String getToken() {
         return this.jwttoken;
+    }
+
+    public String getId_user() {
+        return id_user;
     }
 }
